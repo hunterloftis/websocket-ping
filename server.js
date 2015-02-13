da6617e6b3a231cf03de51c31aefb9111f57dd7f
Hunter Loftis <hunter@hunterloftis.com>
2015-02-13 11:30:32 -0800

    add sockjs test

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var sockjs = require('sockjs').createServer();
 
 var PORT = process.env.PORT || 3000;
+
+sockjs.installHandlers(http, { prefix: '/sockjs' });
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', function(socket){
-  console.log('connection established');
+  console.log('socket.io connection established');
+});
+
+sockjs.on('connection', function(conn) {
+  console.log('sockjs connection established');
 });
 
 http.listen(PORT, function(err) {
